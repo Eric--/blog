83f2c1bbe5d7bf96eb6b70d898ec1d16f0a4c0c3
Eric-- <rmb5241@gmail.com>
2014-06-02 09:40:08 +0800

    实现了留言功能

--- models/post.js
+++ models/post.js
@@ -29,7 +29,8 @@ Post.prototype.save = function(funCallback)
 		name: this.name,
 		time: time,
 		title: this.title,
-		post: this.post
+		post: this.post,
+		comments: []
 	};
 	//打开数据库
 	mongodb.open(function(err, db){
@@ -60,7 +61,7 @@ Post.prototype.save = function(funCallback)
 }
 
 //读取文章及其相关信息
-Post.get = function(name, funCallback)
+Post.getAll = function(name, funCallback)
 {
 	//打开数据库
 	mongodb.open(function(err, db){
@@ -88,6 +89,151 @@ Post.get = function(name, funCallback)
 					doc.post = markdown.toHTML(doc.post);
 				});
 				funCallback(null, docs);
+			});
+		});
+	});
+}
+
+//获取一篇文章
+Post.getOne = function(name, day, title, funCallback)
+{
+	//打开数据库
+	mongodb.open(function(err, db){
+		if(err)
+		{
+			return funCallback(err);
+		}
+		//读取 posts集合
+		db.collection('posts', function(err, collection){
+			if(err)
+			{
+				mongodb.close();
+				return funCallback(err);
+			}
+		
+			//根据用户名、发表日期及文章名进行查询
+			collection.findOne({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, function(err, doc){
+				mongodb.close();
+				if(err)
+				{
+					return funCallback(err);
+				}
+				//解析 md 为 html
+				doc.post = markdown.toHTML(doc.post);
+				doc.comments.forEach(function(comment){
+					comment.content = markdown.toHTML(comment.content);
+				});
+				funCallback(null, doc);
+			});
+		});
+	});
+}
+
+//返回原始发表的内容(markdown 格式)
+Post.edit = function(name, day, title, funCallback)
+{
+	//打开数据库
+	mongodb.open(function(err, db){
+		if(err)
+		{
+			return funCallback(err);
+		}
+		//读取 posts集合
+		db.collection('posts', function(err, collection){
+			if(err)
+			{
+				mongodb.close();
+				return funCallback(err);
+			}
+		
+			//根据用户名、发表日期及文章名进行查询
+			collection.findOne({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, function(err, doc){
+				mongodb.close();
+				if(err)
+				{
+					return funCallback(err);
+				}
+				funCallback(null, doc);
+			});
+		});
+	});
+}
+
+//更新一篇文章及相关信息
+Post.update = function(name, day, title, post, funCallback)
+{
+	//打开数据库
+	mongodb.open(function(err, db){
+		if(err)
+		{
+			return funCallback(err);
+		}
+		//读取 posts集合
+		db.collection('posts', function(err, collection){
+			if(err)
+			{
+				mongodb.close();
+				return funCallback(err);
+			}
+		
+			//更新文章内容
+			collection.update({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, {
+				$set: {post: post}
+			}, function(err, doc){
+				mongodb.close();
+				if(err)
+				{
+					return funCallback(err);
+				}
+				funCallback(null);
+			});
+		});
+	});
+}
+
+//删除一篇文章
+Post.remove = function(name, day, title, funCallback)
+{
+	//打开数据库
+	mongodb.open(function(err, db){
+		if(err)
+		{
+			return funCallback(err);
+		}
+		//读取 posts集合
+		db.collection('posts', function(err, collection){
+			if(err)
+			{
+				mongodb.close();
+				return funCallback(err);
+			}
+		
+			//根据用户名、发表日期及文章名删除一篇文章
+			collection.remove({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, {
+				w: 1
+			},function(err, doc){
+				mongodb.close();
+				if(err)
+				{
+					return funCallback(err);
+				}
+				funCallback(null);
 			});
 		});
 	});
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var crypto = require('crypto'),
 	fs = require('fs'),
 	formidable = require("formidable"),
 	User = require('../models/user.js'),
-	Post = require('../models/post.js');
+	Post = require('../models/post.js'),
+	Comment = require('../models/comment.js');
 var express = require('express');
 var router = express.Router();
 
@@ -24,7 +25,7 @@ router.get('/layout', function(req, res) {
 /* blog */
 router.get('/', checkLogin);
 router.get('/', function(req, res) {
-  Post.get(null, function(err, posts){
+  Post.getAll(null, function(err, posts){
   	if(err){
 		posts = [];
 	}
@@ -134,7 +135,7 @@ router.get('/post', function(req, res) {
 router.post('/post', checkLogin);
 router.post('/post', function(req, res) {
   var currentUser = req.session.user,
-      post = new Post(currentUser, req.body.title, req.body.post);
+      post = new Post(currentUser.name, req.body.title, req.body.post);
   post.save(function(err){
   	if(err){
 		req.flash('error', err);
@@ -185,6 +186,134 @@ router.post('/upload', function(req, res){
   	});
 	req.flash('success', '文件上传成功！');
   	res.redirect('/upload');
+});
+
+router.get("/u/:name", function(req, res){
+	
+	//检查用户是否存在
+	User.get(req.params.name, function(err, user){
+		
+		if(!user){
+			req.flash("error", '用户不存在！');
+			return res.redirect('/');
+		}
+		//查询并返回该用户的所有文章
+		Post.getAll(user.name, function(error, posts){
+			
+			if(error){
+				req.flash('error', error);
+				return res.redirect('/');
+			}
+			res.render('user', {
+				title: user.name,
+				posts: posts,
+				user: req.session.user,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
+});
+
+router.get("/u/:name/:day/:title", function(req, res){
+		
+	//查询并返回该用户的所有文章
+	Post.getOne(req.params.name, req.params.day, req.params.title, function(error, post){
+		
+		if(error){
+			req.flash('error', error);
+			return res.redirect('/');
+		}
+		res.render('article', {
+			title: req.params.title,
+			post: post,
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
+	});
+});
+
+router.get('/edit/:name/:day/:title', checkLogin);
+router.get('/edit/:name/:day/:title', function(req, res){
+	
+	var currentUser = req.session.user;
+	Post.edit(currentUser.name, req.params.day, req.params.title, function(err, post){
+		
+		if(err)
+		{
+			req.flash('error', err);
+			res.redirect('back');
+		}
+		res.render('edit', {
+			
+			title: '编辑',
+			post: post,
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
+	});
+});
+
+//修改文章
+router.post('/edit/:name/:day/:title', checkLogin);
+router.post('/edit/:name/:day/:title', function(req, res){
+	
+	var currentUser = req.session.user;
+	Post.update(currentUser.name, req.params.day, req.params.title, req.body.post, function(err){
+		
+		var url = "/u/" + req.params.name + "/" + req.params.day + "/" + req.params.title;
+		if(err)
+		{
+			req.flash('error', err);
+			return res.redirect(url);
+		}
+		req.flash('success', '修改成功！');
+		res.redirect(url);
+	});
+});
+
+//删除文章
+router.get('/remove/:name/:day/:title', checkLogin);
+router.get('/remove/:name/:day/:title', function(req, res){
+	
+	var currentUser = req.session.user;
+	Post.remove(currentUser.name, req.params.day, req.params.title, function(err){
+		
+		if(err)
+		{
+			req.flash('error', err);
+			return res.redirect('back');
+		}
+		req.flash('success', '删除成功！');
+		res.redirect('/');
+	});
+});
+
+//加入留言功能
+router.post('/u/:name/:day/:title', function(req, res){
+	
+	var date = new Date(),
+		time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + 
+				date.getDate() + " " + date.getHours() + ":" + 
+				(date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
+	var comment = {
+		name: req.body.name,
+		email: req.body.email,
+		website: req.body.website,
+		time: time,
+		content: req.body.content
+	}
+	var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+	newComment.save(function(err){
+		if(err){
+			req.flash('error', err);
+			return res.redirect('back');
+		}
+		req.flash('success', '留言成功！');
+		res.redirect('back');
+	});
 });
 
 //页面权限控制
